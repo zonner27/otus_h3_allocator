71e457b64aba175bf82063270b75aed308e437e1
zonner27 <zonner2785@gmail.com>
2019-08-05 15:07:18 +0300

    2

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -4,56 +4,100 @@
 #include <vector>
 #include <map>
 
-template <class T>
+template <class T, std::size_t count>
 struct my_alloc
 {
     using value_type = T;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
 
-    T *allocate(std::size_t n)
+    my_alloc() : m_memory(nullptr), m_count(0) {};
+
+    template <typename U>
+    struct rebind
     {
-        std::cout << "Allocated = " << n << std::endl;
-        auto p = std::malloc(n * sizeof(T));
-        if (!p)
-            throw std::bad_alloc();
-        return reinterpret_cast<T*>(p);
+        using other = my_alloc<U, count>;
+    };
+
+    pointer allocate(std::size_t n)
+    {      
+        if (m_memory == nullptr)
+        {
+            auto p = std::malloc(n * sizeof(value_type) * count);    //auto p
+            if (!p)
+                throw std::bad_alloc();
+            std::cout << "Allocated = " << n * count << std::endl;
+            m_memory = reinterpret_cast<pointer>(p);
+        }
+        else
+            m_memory++;
+        std::cout << m_memory << std::endl;
+        //return reinterpret_cast<pointer>(p);
+        return m_memory;
     }
 
-    void deallocate(T *p, [[maybe_unused]] std::size_t n)
+//    if (bytes > sizeof big_buffer - index) {
+//        throw std::bad_alloc();
+//    }
+//    index += bytes;
+//    return &big_buffer[index - bytes];
+
+    void deallocate(pointer p, [[maybe_unused]] std::size_t n)
     {
-        std::cout << "Deallocated " << std::endl;
-        std::free(p);
+        if (m_memory != nullptr && m_count == count - 1)
+        {
+            std::cout << "Deallocated " << std::endl;
+            std::free(p);
+            m_memory = nullptr;
+        }
+        else
+            m_count++;
     }
 
     template<typename U, typename ...Args>
     void construct(U *p, Args &&...args)
     {
-        std::cout << "Called construct \n";
+        std::cout << "Called construct " << std::endl;
         new(p) U(std::forward<Args>(args)...);
     }
 
-    void destroy(T *p)
+    void destroy(pointer p)
     {
         std::cout << "Called destroy\n";
         p->~T();
     }
+private:
+    pointer m_memory;
+    std::size_t m_count;
+
 };
 
 int main()
 {
-    //std::list<int, my_alloc<int>> a;
-    //a.push_back(123);
 
-//    std::vector<int, my_alloc<int>> b;
-//    b.reserve(10);
-//    b.push_back(1);
-//    b.push_back(2);
 
-    std::map<int, char, std::less<int>, my_alloc<std::pair<const int, char>>> b;
+    std::map<int, int> map_standart;
+//    for(int i = 0; i < 10; ++i)
+//    {
+//        map_standart.insert(std::make_pair(i, i*i));
+//    }
+
+//    for(auto it = map_standart.begin(); it != map_standart.end(); ++it)
+//    {
+//        std::cout << it->first << " " << it->second << std::endl;
+//    }
+
+    std::map<int, int, std::less<int>, my_alloc<std::pair<const int, int>, 10>> map_myallocator;
     for(int i = 0; i < 10; ++i)
     {
-        b.insert(std::make_pair(23, 'g'));
+        map_myallocator.insert(std::make_pair(i, i*i));
     }
 
-
+    for(auto it = map_myallocator.begin(); it != map_myallocator.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second << std::endl;
+    }
     return 0;
 }
