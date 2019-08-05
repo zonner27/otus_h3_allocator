4e040c43673899e1712de468776367450d30a98a
zonner27 <zonner2785@gmail.com>
2019-08-05 23:16:33 +0300

    аллокатор

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <typeinfo>
-#include <list>
-#include <vector>
 #include <map>
+
 
 template <class T, std::size_t count>
 struct my_alloc
@@ -28,44 +26,46 @@ struct my_alloc
             auto p = std::malloc(n * sizeof(value_type) * count);    //auto p
             if (!p)
                 throw std::bad_alloc();
-            std::cout << "Allocated = " << n * count << std::endl;
+            //std::cout << "p = " << p << std::endl;
+            //std::cout << "Allocated = " << n * count << std::endl;
             m_memory = reinterpret_cast<pointer>(p);
+            //std::cout << m_memory << std::endl;
+            return m_memory;
         }
         else
-            m_memory++;
-        std::cout << m_memory << std::endl;
-        //return reinterpret_cast<pointer>(p);
+        {
+            m_count++;
+            if (m_count >= count)
+                throw std::bad_alloc();
+            //std::cout << (m_memory + m_count) << std::endl;
+            return (m_memory + m_count);
+        }
         return m_memory;
     }
 
-//    if (bytes > sizeof big_buffer - index) {
-//        throw std::bad_alloc();
-//    }
-//    index += bytes;
-//    return &big_buffer[index - bytes];
-
     void deallocate(pointer p, [[maybe_unused]] std::size_t n)
     {
-        if (m_memory != nullptr && m_count == count - 1)
+        //std::cout << "p = " << p << " m_count " << m_count << std::endl;
+        if (m_memory != nullptr && m_count == 0)
         {
-            std::cout << "Deallocated " << std::endl;
+            //std::cout << "Deallocated " << std::endl;
             std::free(p);
             m_memory = nullptr;
         }
         else
-            m_count++;
+            m_count--;
     }
 
     template<typename U, typename ...Args>
     void construct(U *p, Args &&...args)
     {
-        std::cout << "Called construct " << std::endl;
+        //std::cout << "Called construct " << std::endl;
         new(p) U(std::forward<Args>(args)...);
     }
 
     void destroy(pointer p)
     {
-        std::cout << "Called destroy\n";
+        //std::cout << "Called destroy\n";
         p->~T();
     }
 private:
@@ -79,15 +79,15 @@ int main()
 
 
     std::map<int, int> map_standart;
-//    for(int i = 0; i < 10; ++i)
-//    {
-//        map_standart.insert(std::make_pair(i, i*i));
-//    }
+    for(int i = 0; i < 10; ++i)
+    {
+        map_standart.insert(std::make_pair(i, i*i));
+    }
 
-//    for(auto it = map_standart.begin(); it != map_standart.end(); ++it)
-//    {
-//        std::cout << it->first << " " << it->second << std::endl;
-//    }
+    for(auto it = map_standart.begin(); it != map_standart.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second << std::endl;
+    }
 
     std::map<int, int, std::less<int>, my_alloc<std::pair<const int, int>, 10>> map_myallocator;
     for(int i = 0; i < 10; ++i)
@@ -99,5 +99,6 @@ int main()
     {
         std::cout << it->first << " " << it->second << std::endl;
     }
+
     return 0;
 }
