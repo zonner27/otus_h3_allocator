227a749d099b648c519da5589000702fc488fc68
zonner27 <zonner2785@gmail.com>
2019-10-09 12:30:04 +0300

    Revert "remove all files for review"
    This reverts commit f7196545407f080a4e82110d6f5b4edf08d29f78.

---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include "allocator.h"
+#include "container.h"
+
+int factorial(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+int main()
+{
+    try {
+        // map standart
+        std::map<int, int> map_standart;
+        for(int i = 0; i < 10; ++i)
+            map_standart.insert(std::make_pair(i, factorial(i)));
+
+        for(auto it = map_standart.begin(); it != map_standart.end(); ++it)
+            std::cout << it->first << " " << it->second << std::endl;
+
+        //map with my allocator
+        std::map<int, int, std::less<int>, My_Alloc<std::pair<const int, int>, 10>> map_myallocator;
+        for(int i = 0; i < 10; ++i)
+            map_myallocator.insert(std::make_pair(i, factorial(i)));
+
+        for(auto it = map_myallocator.begin(); it != map_myallocator.end(); ++it)
+            std::cout << it->first << " " << it->second << std::endl;
+
+        //my list
+        MyList<int> mlst;
+        for (int i = 0; i < 10; i++)
+            mlst.push_back(i);
+
+        for (auto i = mlst.begin(); i != mlst.end(); i++)
+            std::cout << *i << std::endl;
+
+        //my list with my allocator
+        MyList<int, My_Alloc<int, 10>> mlsta;
+        for (int i = 0; i < 10; i++)
+            mlsta.push_back(i);
+
+        for (auto i = mlsta.begin(); i != mlsta.end(); i++)
+            std::cout << *i << std::endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ tests/test_alloc.cpp
@@ -0,0 +1,35 @@
+#define BOOST_TEST_MODULE test_main
+
+#include "allocator.h"
+#include "container.h"
+#include <iostream>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_main)
+
+BOOST_AUTO_TEST_CASE(test_container)
+{
+    MyList<int> mlst;
+    mlst.push_back(1);
+    mlst.push_back(2);
+    mlst.push_back(3);
+    auto it = mlst.begin();
+    BOOST_REQUIRE(*it == 1);
+    BOOST_REQUIRE(*(++it) == 2);
+    BOOST_REQUIRE(*(++it) == 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_alloc)
+{
+    MyList<int, My_Alloc<int, 3>> mlsta;
+    mlsta.push_back(1);
+    mlsta.push_back(2);
+    mlsta.push_back(3);
+    auto it = mlsta.begin();
+    BOOST_REQUIRE(*it == 1);
+    BOOST_REQUIRE(*(++it) == 2);
+    BOOST_REQUIRE(*(++it) == 3);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
